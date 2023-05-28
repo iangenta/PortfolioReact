c05c41eed4e3f90642cd32bac42052930d83a398
ian genta <gentaian2@gmail.com>
2023-05-28 14:28:04 -0300

    problems have been fixed.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,115 @@
 import React from 'react';
+import useForm from '../assets/useForm';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
 
 const Contact = () => {
-  return <div className='section' id='contact'>Contact</div>;
+  const initialData = {
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  }
+
+  const onValidate = (form) => {
+    let errors = {}
+    let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+    let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+    let regexComments = /^.{1,255}$/;
+
+    if (!form.name.trim()) {
+      errors.name = 'name cannot be empty'
+    } else if (!regexName.test(form.name)){
+      errors.name = 'the name field accepts only letters and spaces'
+    }
+
+    if (!form.email.trim()) {
+      errors.email = 'email cannot be empty'
+    } else if (!regexEmail.test(form.email)){
+      errors.email = 'mail does not have a valid format'
+    }
+
+    if (!form.subject.trim()) {
+      errors.subject = "cannot be empty"
+    } else if (!regexName.test(form.subject)){
+      errors.subject = 'the subject field accepts only letters and spaces'
+    }
+
+    if (!form.message.trim()) {
+      errors.message = 'message cannot be empty'
+    } else if (!regexComments.test(form.message)){
+      errors.message = 'This field accepts only 255 characters'
+    }
+
+    return errors
+  }
+
+  const { form, errors, handleChange, handleSubmit } = useForm(initialData, onValidate)
+
+
+  return (
+
+    <section className=' py-16 lg:section ' id='contact'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col lg:flex-row'>
+
+          <motion.div
+            variants={fadeIn('right', 0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1'>
+
+            <div>
+              <h4 className='text-xl uppercase text-accent font-medium mb-2 tracking-wide'>Get in touch</h4>
+              <h2 className='text-[45px] lg:text-[90x] leading-none mb-12'>Let's talk about <br />your projects!</h2>
+
+
+            </div>
+
+
+          </motion.div>
+
+
+
+          <motion.form
+            variants={fadeIn('left', 0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className=' flex-1' action='https://formsubmit.co/fd170b4a678287d427d37db88ff3b3af' onSubmit={handleSubmit} method='POST' >
+
+            <input className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all' type='email' name='email' placeholder='Your email' onChange={handleChange} />
+            {errors && errors.email && <span className='text-accent'>{errors.email}</span>}
+
+            <input className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all' type='text' name='name' placeholder='Your name' value={form.name} onChange={handleChange} />
+            {errors && errors.name && <span className='text-accent'>{errors.name}</span>}
+            <input className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all' type='text' name='subject' placeholder='Subject'  value={form.subject} onChange={handleChange} />
+            {errors && errors.subject && <span className='text-accent'>{errors.subject}</span>}
+            <textarea className='bg-transparent border-b py-12 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-12' placeholder='Your message'  value={form.message} name='message' onChange={handleChange} ></textarea>
+            {errors && errors.message && <span className='text-accent'>{errors.message}</span>}
+            <button className='btn btn-lg'>Send Message </button>
+            <input type="hidden" name="_next" value="http://localhost:3000"></input>
+            <input type="hidden" name="_captcha" value="false"></input>
+          </motion.form>
+
+
+
+
+        </div>
+
+      </div>
+
+
+
+
+
+
+    </section>
+
+
+
+  );
 };
 
 export default Contact;
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,7 +1,103 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
+import imagen1 from '../assets/onlinebk.png';
+import imagen2 from '../assets/itbank.png';
+import imagen3 from '../assets/jvnts.png';
+
 
 const Work = () => {
-  return <div className='section' id='work'>Work</div>;
+  return (
+    <section id='work' className='py-10'>
+      <div className='container mx-auto'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-12'>
+          <motion.div
+            variants={fadeIn('right', 0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
+            {/* text */}
+            <h2 className='h2 leading-tight text-accent'>My Latest <br /> Work</h2>
+            <p className='max-w-sm mb-16 lg:mb-0'>simple preview of some of my recent projects. 
+All of them were designed and launched by me </p>
+            <div><button className='btn btn-sm'>View all projects</button></div>
+          </motion.div>
+
+          <motion.div
+            variants={fadeIn('left', 0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 flex flex-col gap-y-10'>
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'>
+              </div>
+
+              <img src={imagen1} alt="" className='group-hover:scale-125 transition-all duration-500' />
+              {/* pre-title */}
+              <div className='absolute bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-gradient'>UI/UX Design & Front end Development</span>
+              </div>
+
+              <div className='absolute bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className='text-3xl text-white'>Online banking</span>
+              </div>
+            </div>
+          </motion.div>
+
+          <motion.div
+            variants={fadeIn('right', 0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 flex flex-col gap-y-10'>
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'>
+              </div>
+
+              <img src={imagen2} alt="" className='group-hover:scale-125 transition-all duration-500' />
+
+              <div className='absolute bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-gradient'>UI/UX Design & Fullstack Development</span>
+              </div>
+
+              <div className='absolute bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className='text-3xl text-white'>Online banking</span>
+              </div>
+            </div>
+          </motion.div>
+
+          <motion.div
+            variants={fadeIn('left', 0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 flex flex-col gap-y-10'>
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+
+
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'>
+              </div>
+
+
+              <img src={imagen3} alt="" className='group-hover:scale-125 transition-all duration-500' />
+
+
+              <div className='absolute bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-gradient'>UI/UX Design & Front end Development</span>
+              </div>
+
+              <div className='absolute bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className='text-3xl text-white'>Juventus Academy Page</span>
+              </div>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section >);
 };
 
 export default Work;
